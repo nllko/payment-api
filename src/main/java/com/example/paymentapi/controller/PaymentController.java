5bdde69af285cfe6d1b4dc5e5cd45f50d79cd37c
f0rsakenOne <nikolaj.aleksejenkov@gmail.com>
2022-05-21 17:03:16 +0300

    added creation of payments through csv

--- src/main/java/com/example/paymentapi/controller/PaymentController.java
+++ src/main/java/com/example/paymentapi/controller/PaymentController.java
@@ -3,10 +3,26 @@ package com.example.paymentapi.controller;
 import com.example.paymentapi.models.Payment;
 import com.example.paymentapi.models.PaymentRequest;
 import com.example.paymentapi.service.PaymentService;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+import com.univocity.parsers.common.record.Record;
+import com.univocity.parsers.csv.CsvParser;
+import com.univocity.parsers.csv.CsvParserSettings;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 import javax.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 public class PaymentController {
@@ -18,9 +34,25 @@ public class PaymentController {
   }
 
   @PostMapping("/payments")
+  @ResponseStatus(HttpStatus.CREATED)
   public Payment savePayment(@Valid @RequestBody PaymentRequest paymentRequest) {
     Payment payment = paymentRequest.toDomain();
     paymentService.savePayment(payment);
     return payment;
+  }
+
+  @PostMapping("/payment-files")
+  @ResponseStatus(HttpStatus.CREATED)
+  public String savePaymentsFromCsv(@RequestParam("file") MultipartFile file) throws Exception {
+    CsvParserSettings settings = new CsvParserSettings();
+    settings.setHeaderExtractionEnabled(true);
+    CsvParser parser = new CsvParser(settings);
+    parser.parseAllRecords(file.getInputStream()).forEach(record -> {
+      PaymentRequest paymentRequest = new PaymentRequest(
+          record.getString("amount"),
+          record.getString("debtorIban"));
+      paymentService.savePayment(paymentRequest.toDomain());
+    });
+    return "success";
   }
 }
--- src/main/java/com/example/paymentapi/service/PaymentService.java
+++ src/main/java/com/example/paymentapi/service/PaymentService.java
@@ -2,6 +2,7 @@ package com.example.paymentapi.service;
 
 import com.example.paymentapi.models.Payment;
 import com.example.paymentapi.repository.PaymentDatabaseRepository;
+import java.util.List;
 import org.springframework.stereotype.Service;
 
 @Service
