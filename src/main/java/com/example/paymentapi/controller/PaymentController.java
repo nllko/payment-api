0cad7879bc50d4f413426b31908e315e1a4b4b33
f0rsakenOne <nikolaj.aleksejenkov@gmail.com>
2022-05-21 17:24:02 +0300

    added method to get list of all payments with an optional parameter

--- src/main/java/com/example/paymentapi/controller/PaymentController.java
+++ src/main/java/com/example/paymentapi/controller/PaymentController.java
@@ -15,8 +15,10 @@ import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import javax.validation.Valid;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -54,5 +56,14 @@ public class PaymentController {
       paymentService.savePayment(paymentRequest.toDomain());
     });
     return "success";
+  }
+
+  @GetMapping("/payments")
+  public List<Payment> getPayments(@RequestParam Optional<String> debtorIban) {
+    if (debtorIban.isPresent()) {
+      return paymentService.getPaymentsByIban(debtorIban.get());
+    } else {
+      return paymentService.getAllPayments();
+    }
   }
 }
--- src/main/java/com/example/paymentapi/service/PaymentService.java
+++ src/main/java/com/example/paymentapi/service/PaymentService.java
@@ -3,6 +3,7 @@ package com.example.paymentapi.service;
 import com.example.paymentapi.models.Payment;
 import com.example.paymentapi.repository.PaymentDatabaseRepository;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,5 +17,15 @@ public class PaymentService {
 
   public void savePayment(Payment payment) {
     paymentDatabaseRepository.save(payment);
+  }
+
+  public List<Payment> getAllPayments() {
+    return paymentDatabaseRepository.findAll();
+  }
+
+  public List<Payment> getPaymentsByIban(String iban) {
+    return paymentDatabaseRepository.findAll().stream()
+        .filter(payment -> payment.getDebtorIban().equalsIgnoreCase(iban))
+        .toList();
   }
 }
